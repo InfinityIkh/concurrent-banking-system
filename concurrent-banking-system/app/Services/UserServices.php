e6109a30dcd04fe4e251e3da943331b4109d4b8e
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-02 21:00:08 +0200

    feat: add user service

--- concurrent-banking-system/app/Services/UserServices.php
+++ concurrent-banking-system/app/Services/UserServices.php
@@ -2,7 +2,77 @@
 
 namespace App\Services;
 
+use App\Http\Resources\UserResources;
+use App\Models\User;
+
 class UserServices
 {
+    //
+    public function createUser(array $credentials): User
+    {
+        //
+        return User::create($credentials);
+    }
 
+    public function updateUser(?User $user, array $credentials): array
+    {
+        //
+        if(!$user){
+            return [
+                'status' => 404,
+                'body' => [
+                    "message" => "User not found",
+                ]
+            ];
+        }
+
+        $user->update($credentials);
+
+        return [
+            'status' => 200,
+            'body' => [
+                'message' => "User updated successfully",
+                'user' => new UserResources($user),
+            ]
+        ];
+    }
+
+    public function deleteUser(?User $user): array
+    {
+        //
+        if(!$user){
+            return [
+                'status' => 404,
+                'body' => [
+                    "message" => "User not found",
+                ]
+            ];
+        }
+        $user->delete();
+        return [
+            'status' => 204,
+            'body' => [
+                "message" => "User deleted successfully",
+            ]
+        ];
+    }
+
+    public function showUser(?User $user): array
+    {
+        if(!$user){
+            return [
+                'status' => 404,
+                'body' => [
+                    "message" => "User not found",
+                ]
+            ];
+        }
+        return [
+            'status' => 200,
+            'body' => [
+                "message" => "User retrieved successfully",
+                "user' => new UserResources($user),"
+            ]
+        ];
+    }
 }
