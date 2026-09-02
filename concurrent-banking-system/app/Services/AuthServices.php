b632a43da4bef0f7836bfe307e1daab5c8f7d9c8
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-02 20:27:05 +0200

    feat: add admin middleware

---
+++ concurrent-banking-system/app/Services/AuthServices.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Resources\UserResources;
+use App\Models\User;
+
+class AuthServices
+{
+    //
+    public function register(array $credentials): array
+    {
+        //
+        $user = User::create($credentials);
+
+        return [
+            'message' => 'User Registered successfully',
+            'user' => new UserResources($user)
+        ];
+    }
+
+    public function login(array $credentials): array
+    {
+        if (! $token = auth('api')->attempt($credentials)) {
+            return [
+                'status' => 401,
+                'body' => [
+                    'error' => 'Unauthorized'
+                ]
+            ];
+        }
+        return [
+            'status' => 200,
+            'body' => [
+                'message' => 'User Login successfully',
+                'data' => $this->respondWithToken($token),
+            ]
+        ];
+    }
+
+    protected function respondWithToken(string $token): array
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ];
+    }
+}
