60dfa59ba662b6859910847675f02b52f62a679c
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-02 20:30:55 +0200

    feat: add user request validation

---
+++ concurrent-banking-system/app/Http/Requests/UserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'firstname' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'lastname' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'email' => [
+                $this->isMethod('POST') ? 'required' : 'nullable',
+                'required',
+                'string',
+                'email',
+                'max:255',
+                $this->is('*/login') ? '' : 'unique:users'
+            ],
+            'password' => [
+                $this->isMethod('POST') ? 'required' : 'nullable',
+                'string',
+                'min:8',
+                'confirmed'
+            ],
+        ];
+    }
+}
