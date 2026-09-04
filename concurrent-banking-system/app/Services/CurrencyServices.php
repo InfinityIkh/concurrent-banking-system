26947707ab1029b35d8a538543288a07b7522d87
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-04 23:17:27 +0200

    feat: add currency services

--- concurrent-banking-system/app/Services/CurrencyServices.php
+++ concurrent-banking-system/app/Services/CurrencyServices.php
@@ -2,7 +2,78 @@
 
 namespace App\Services;
 
+use App\Models\Currency;
+use App\Http\Resources\CurrencyResource;
+
 class CurrencyServices
 {
+    //
+    public function get(?Currency $currency): array
+    {
+        if(!$currency) {
+            return [
+                'status' => 404,
+                'body' => [
+                    'message' => 'Currency not found',
+                ]
+            ];
+        }
+        return [
+            'status' => 200,
+            'body' => [
+                'currency' => new CurrencyResource($currency),
+            ]
+        ];
+    }
 
+    public function create(array $credentials): array
+    {
+        $currency = Currency::create($credentials);
+        return [
+            'status' => 201,
+            'body' => [
+                'message' => 'Successfully created Currency',
+                'currency' => new CurrencyResource($currency)
+            ]
+        ];
+    }
+
+    public function update(array $credentials ,?Currency $currency): array
+    {
+        if(!$currency) {
+            return [
+                'status' => 404,
+                'body' => [
+                    'message' => 'Currency not found',
+                ]
+            ];
+        }
+        $currency->update($credentials);
+        return [
+            'status' => 200,
+            'body' => [
+                'message' => 'Successfully updated Currency',
+                'currency' => new CurrencyResource($currency),
+            ]
+        ];
+    }
+
+    public function delete(?Currency $currency): array
+    {
+        if(!$currency) {
+            return [
+                'status' => 404,
+                'body' => [
+                    'message' => 'Currency not found',
+                ]
+            ];
+        }
+        $currency->delete();
+        return [
+            'status' => 200,
+            'body' => [
+                'message' => 'Successfully deleted Currency',
+            ]
+        ];
+    }
 }
