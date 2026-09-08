5150c4a4eebc5e005304cf1ff371d09a747ae011
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-08 18:08:15 +0200

    feat: add transaction controller

---
+++ concurrent-banking-system/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TransactionRequest;
+use App\Models\Account;
+use App\Models\Transaction;
+use App\Services\TransactionServices;
+use Illuminate\Http\JsonResponse;
+
+class TransactionController extends Controller
+{
+    //
+    public function __construct(public TransactionServices $transactionServices){}
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResponse
+    {
+        //
+        $response = $this->transactionServices->getTransactions();
+        return response()->json(
+            $response['body'],$response['status']
+        );
+    }
+
+
+
+    public function show(Transaction $transaction): JsonResponse
+    {
+        //
+        $response = $this->transactionServices->getTransaction($transaction);
+        return response()->json(
+            $response['body'],$response['status']
+        );
+    }
+
+    public function update(TransactionRequest $request, Transaction $transaction): JsonResponse
+    {
+        //
+        $credentials = $request->validated();
+        $response = $this->transactionServices->updateTransaction($credentials ,$transaction);
+        return response()->json(
+            $response['body'],$response['status']
+        );
+    }
+
+    public function destroy(Transaction $transaction): JsonResponse
+    {
+        //
+        $response = $this->transactionServices->removeTransaction($transaction);
+        return response()->json(
+            $response['body'],$response['status']
+        );
+    }
+}
