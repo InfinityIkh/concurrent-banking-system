317578f642cf64dbf6777645747b2c037ed902c0
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-10 23:01:35 +0200

    feat: add API version resolution endpoint

--- concurrent-banking-system/routes/api.php
+++ concurrent-banking-system/routes/api.php
@@ -1,30 +1,6 @@
 <?php
 
-use App\Http\Controllers\AccountController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CurrencyController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-//
-Route::post('auth/login', [AuthController::class ,'login']);
-Route::post('auth/register', [AuthController::class ,'register']);
-//
-Route::middleware('auth:api')->group(function () {
-    Route::get('/profile', function (Request $request) {
-        return $request->user();
-    });
-    Route::middleware(['admin','account.active'])->group(function () {
-        Route::apiResource('users' , UserController::class);
-        Route::apiResource('currencies' ,CurrencyController::class);
-        Route::get('accounts' , [AccountController::class ,'index']);
-        Route::post('accounts/{user}', [AccountController::class, 'store']);
-        Route::put('accounts/{account}', [AccountController::class, 'update']);
-        Route::get('accounts/{account}', [AccountController::class, 'show']);
-        Route::delete('accounts/{account}', [AccountController::class, 'destroy']);
-        Route::patch('accounts/{id}/activate', [AccountController::class, 'activateAccount']);
-        Route::patch('accounts/{id}/deactivate', [AccountController::class, 'deactivateAccount']);
-        Route::patch('accounts/{id}/close', [AccountController::class, 'closeAccount']);
-    });
+Route::prefix('v1')->group(function () {
+    require __DIR__.'/api_v1.php';
 });
