26fa68ac66a5c39d35110e20a7a6609418d62f0b
L1m1tlesAyman <limtgojo@gmail.com>
2026-09-11 22:58:28 +0200

    feat: add document generator service

--- concurrent-banking-system/app/Services/DocumentGeneratorService.php
+++ concurrent-banking-system/app/Services/DocumentGeneratorService.php
@@ -2,7 +2,20 @@
 
 namespace App\Services;
 
+use App\Models\Transaction;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Storage;
+
 class DocumentGeneratorService
 {
-
+    public function generatePdf(Transaction $transaction): string
+    {
+        $pdf = Pdf::loadView('transaction.receipt',
+            [
+                'transaction' => $transaction,
+            ]);
+        $fileName = 'receipts/'.$transaction->id.'.pdf';
+        Storage::disk('public')->put($fileName, $pdf->output());
+        return Storage::disk('public')->url($fileName);
+    }
 }
